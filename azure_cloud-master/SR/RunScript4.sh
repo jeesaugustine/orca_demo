44f88655e7962f859ae9e2ab8562dbbee92b351b
Jees Augustine <augustine.jees@yahoo.com>
2020-07-08 15:51:05 -0500

    removal of some redundant files

--- azure_cloud-master/SR/RunScript4.sh
+++
@@ -1,5 +0,0 @@
-./SR -f data/200ke/8M/ -o outpute.txt
-./SR -f data/200ke/16M/ -o outpute.txt
-./SR -f data/200ke/32M/ -o outpute.txt
-./SR -f data/200ke/64M/ -o outpute.txt
-./SR -f data/200ke/128M/ -o outpute.txt
